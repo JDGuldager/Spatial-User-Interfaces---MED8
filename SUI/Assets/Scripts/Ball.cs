1c223f5d7a9124be534c0634031904de8c0aec4f
Laurids Jacobsen <115459956+laurman1@users.noreply.github.com>
2026-03-13 20:34:00 +0100

    Add files via upload

---
+++ SUI/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class Ball : MonoBehaviour
+{
+    [SerializeField] MeshRenderer meshRenderer;
+    [SerializeField] GameObject effect;
+    [SerializeField] GameObject onImpact;
+    [SerializeField] private float destroyDelay = 2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Debug.Log("Ball Start");
+        if (onImpact != null)
+        {
+            onImpact.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            meshRenderer.enabled = false;
+            effect.SetActive(false);
+            onImpact.SetActive(true);
+            
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+}
---
+++ SUI/Assets/Scripts/Fire.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Fire : Ball
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
+    // Update is called once per frame
+    
+}
